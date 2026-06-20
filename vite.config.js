e25ade3da05c1a8ed4f963332a136af85c9fb143
Fachri Ibnu Falah <fahriibnu1234@gmail.com>
2026-06-20 16:16:31 +0700

    feat: add Docker support and update proxy configuration for containerized development

--- vite.config.js
+++ vite.config.js
@@ -9,13 +9,14 @@ export default defineConfig({
     tailwindcss(),
   ],
   server: {
+    host: true,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://host.docker.internal:8000',
         changeOrigin: true,
       },
       '/storage': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://host.docker.internal:8000',
         changeOrigin: true,
       }
     }
